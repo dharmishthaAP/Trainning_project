0a110dbc00a993bb66c8f897266ac4fe88b1649d
dharmishthaAP <112606307+dharmishthaAP@users.noreply.github.com>
2022-09-20 13:46:37 +0530

    removed api's

--- server/Schema.sql
+++ server/Schema.sql
@@ -24,15 +24,3 @@ CREATE TABLE employee_project_details (
     CONSTRAINT employee_project_details_pkey PRIMARY KEY (emp_id, project_id)
 ); 
 
-API = {
-    GET http://localhost:5000/employee
-    GET http://localhost:5000/employee/id
-    POST http://localhost:5000/employee
-    DELETE http://localhost:5000/employee/id
-    PATCH http://localhost:5000/employee/id
-    GET http://localhost:5000/project
-    GET http://localhost:5000/project/id
-    POST http://localhost:5000/project
-    DELETE http://localhost:5000/project/id
-    PATCH http://localhost:5000/project/id
-}
